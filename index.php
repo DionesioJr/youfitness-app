3336169b3291591ba2eadfe2b332b6b05d8e7d18
root <root@vps30730.publiccloud.com.br>
2021-05-28 22:12:16 -0300

    correçao session_status

--- index.php
+++ index.php
@@ -1,4 +1,9 @@
 <?php
+if (session_status() == PHP_SESSION_NONE) {
+    session_cache_expire(43200); // 30 dias
+    session_start();
+}
+
 ob_start();
 require_once("core/routes.php");
 ob_end_flush();
